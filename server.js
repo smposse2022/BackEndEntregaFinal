a01f0bb3678c902dd4a837aea921595aaa0c2309
Santiago Posse <smposse@gmail.com>
2023-03-15 19:45:39 -0300

    Actualizando Repo

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 import express from "express";
-import { Server } from "socket.io";
-import { options } from "./src/config/options.js";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import session from "express-session";
@@ -12,15 +10,6 @@ import os from "os";
 import { logger } from "./src/logger.js";
 import { apiRouter } from "./src/routes/indexRouter.js";
 import { objArguments } from "./src/config/options.js";
-
-import { getApiDao } from "./src/dbOperations/index.js";
-
-const {
-  CartDaoContainer,
-  MessagesDaoContainer,
-  ProductsDaoContainer,
-  UsersDaoContainer,
-} = await getApiDao(options.server.DBTYPE);
 
 // Crear el servidor
 const app = express();
@@ -73,36 +62,6 @@ if (MODO == "CLUSTER" && cluster.isPrimary) {
   const server = app.listen(PORT, () =>
     logger.info(`listening on port ${PORT} on process ${process.pid}`)
   );
-  const io = new Server(server);
-  const productsApi = ProductsDaoContainer;
-  const messagesApi = MessagesDaoContainer;
-
-  /*//socket
-  io.on("connection", async (socket) => {
-    logger.info("nuevo usuario conectado", socket.id);
-
-    //enviar todos los productos
-    socket.emit("products", await productsApi.getAll());
-
-    //agrego el producto a la lista de productos
-    socket.on("newProduct", async (data) => {
-      const info = await productsApi.save(data);
-      logger.info(info);
-      //envío la lista de productos actualizada a todos los sockets
-      io.sockets.emit("products", await productsApi.getAll());
-    });
-
-    //CHAT
-    //Envio de todos los mensajes al socket que se conecta.
-    io.sockets.emit("messages", await messagesApi.getAll());
-
-    //recibimos el mensaje del usuario y lo guardamos en el archivo chat.txt
-    socket.on("newMessage", async (newMsg) => {
-      await messagesApi.save(newMsg);
-
-      io.sockets.emit("messages", await messagesApi.getAll());
-    });
-  });*/
 }
 
 //api routes
